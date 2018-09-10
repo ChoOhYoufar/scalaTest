fd6d65fc4a5c7135606d8c156f01c3bc60afeb20
ChoOhYoufar <cho.oh@bizreach.co.jp>
2018-09-10 14:20:37 +0900

    complete ::

---
+++ src/main/scala/OhList.scala
@@ -0,0 +1,53 @@
+/**
+  * @author cho.oh 西暦18/07/11.
+  */
+trait MyListTrait[+A] {
+  val head: A
+  val tail: MyListTrait[A]
+  def ::[B >: A](b: B) = MyList(b, this)
+  //TODO 为什么用[B >: A](b: B)
+//  def :::(b: MyList) = MyList(b, this)
+
+//  def ::{}
+  def :::[B >: A](b: MyList[B]) = MyList(b, this)
+  //
+//  def map
+//  def flatMap
+//  def reverse
+//
+//  val days = List("a", "b")
+//  days.head
+
+//  def map = this match {
+//  case MyList
+//  case MyNil
+//}
+
+}
+
+case class MyList[A] (head: A, tail: MyListTrait[A]) extends MyListTrait[A] {
+//  override def ::: : Unit = ???
+}
+
+case object MyNil extends MyListTrait[Nothing] {
+  override val head = throw new Exception
+  override val tail = throw new Exception
+
+//  override def ::: : Unit = ???
+}
+
+MyList(1, MyList(2, MyList(3, MyNil)))
+1::MyList(2, MyNil)
+MyList(1, MyNil):::MyList(2, MyNil)
+
+
+//sealed trait OhList[+A]
+//case object Nil extends OhList[Nothing]
+//case class Cons[+A](ohListHead: A, ohListTail: OhList[A]) extends OhList[A]
+//
+//object List {
+//  def append[A](a1: List[A], a2: List[A]): a1 match {
+//    case Nil => a2
+//    case Cons(h, t) => Cons(h, append(t, a2))
+//  }
+//}
